0e50e9ab197da232672916631837232f8bd36a83
Rik Chilvers <rikchilvers@fastmail.com>
2021-01-04 14:29:25 +0000

    whitespace parsing with nom

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,47 @@
+extern crate nom;
+
+use nom::{
+    bytes::complete::is_a,
+    // character::complete::space0,
+    // character::is_space,
+    // combinator::map,
+    // error::{context, ErrorKind, VerboseError},
+    // multi::{count, many0},
+    IResult,
+};
 use std::io::BufRead;
 
 fn main() {
-    let path = "tests/test.journal";
+    // let path = "tests/test.journal";
+    let path = "/Users/rik/Desktop/Untitled.txt";
     let mut lexer = Lexer::new();
     lexer.lex(path);
 }
 
 #[derive(Default)]
 struct Lexer {
-    line: String,
+    state: LexerState,
+    field: Vec<char>,
     line_number: u64,
     column: u64,
 }
 
+enum LexerState {
+    ConsumingSpace,
+}
+
+impl Default for LexerState {
+    fn default() -> Self {
+        LexerState::ConsumingSpace
+    }
+}
+
 impl Lexer {
     fn new() -> Self {
-        Default::default()
+        Lexer {
+            column: 1,
+            ..Default::default()
+        }
     }
 
     fn lex(&mut self, path: &str) -> bool {
@@ -24,6 +50,8 @@ impl Lexer {
 
         for line in reader.lines() {
             self.line_number += 1;
+            self.field.clear();
+
             match line {
                 Ok(l) => {
                     if !self.lex_line(l) {
@@ -42,15 +70,37 @@ impl Lexer {
         return true;
     }
 
-    fn lex_line(&self, line: String) -> bool {
-        for c in line.chars() {
-            print!("{}", c);
+    fn lex_line(&mut self, line: String) -> bool {
+        if line.len() == 0 {
+            return true;
         }
-        println!();
+
+        // let spaces = count_whitespace(&line);
+        let spaces = count_whitespace_multi(&line);
+
+        if let Ok(s) = spaces {
+            println!("{} spaces followed by {}", s.1, s.0);
+        }
+
         true
     }
+}
 
-    fn next(&self) -> Option<char> {
-        Some('c')
-    }
+fn count_whitespace_multi(i: &str) -> IResult<&str, u64> {
+    nom::multi::fold_many0(count_whitespace, 0, |mut acc: u64, item| {
+        acc += item;
+        acc
+    })(i)
+}
+
+fn count_whitespace(i: &str) -> IResult<&str, u64> {
+    nom::branch::alt((count_spaces, count_tabs))(i)
+}
+
+fn count_spaces(i: &str) -> IResult<&str, u64> {
+    nom::combinator::map(is_a("\t"), |s: &str| (s.len() * 2) as u64)(i)
+}
+
+fn count_tabs(i: &str) -> IResult<&str, u64> {
+    nom::combinator::map(is_a(" "), |s: &str| (s.len() as u64))(i)
 }
