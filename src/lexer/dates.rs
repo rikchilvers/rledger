20375ed449776f08af1e562a0ba94c20bc0c0df1
Rik Chilvers <rikchilvers@fastmail.com>
2021-01-05 09:51:51 +0000

    improve date lexing

--- src/lexer/dates.rs
+++ src/lexer/dates.rs
@@ -1,21 +1,31 @@
-use nom::{character::complete::*, combinator::map, multi::*, IResult};
+use nom::{
+    bytes::complete::is_a,
+    character::complete::digit1,
+    combinator::{map_res, opt, recognize},
+    sequence::preceded,
+    IResult,
+};
 
-fn digit4(i: &str) -> IResult<&str, Vec<&str>> {
-    many_m_n(4, 4, digit1)(i)
+fn date(i: &str) -> IResult<&str, String> {
+    let (i, date_first) = digit_many(i)?;
+
+    let (i, maybe_date_second) = opt(preceded(is_a("/-."), digit_many))(i)?;
+
+    if let Some(date_second) = maybe_date_second {
+        let (i, maybe_date_third) = opt(preceded(is_a("/-."), digit_many))(i)?;
+
+        if let Some(date_third) = maybe_date_third {
+            return Ok((i, format!("{}-{}-{}", date_first, date_second, date_third)));
+        }
+
+        return Ok((i, format!("{}-{}", date_first, date_second)));
+    }
+
+    Ok((i, format!("{}", date_first)))
 }
 
-fn digit_many(i: &str) -> IResult<&str, Result<u64, nom::error::Error<&str>>> {
-    map(
-        nom::bytes::complete::take_while(is_digit_char),
-        |s: &str| {
-            s.parse::<u64>()
-                .map_err(|_| nom::error::Error::new(i, nom::error::ErrorKind::ParseTo))
-        },
-    )(i)
-}
-
-fn is_digit_char(c: char) -> bool {
-    c.is_ascii() && nom::character::is_digit(c as u8)
+fn digit_many(i: &str) -> IResult<&str, u64> {
+    map_res(recognize(digit1), str::parse)(i)
 }
 
 #[cfg(test)]
@@ -23,15 +33,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_finds_four_digits() {
-        assert_eq!(digit_many("2021"), Ok(("", Ok(2021))));
-        assert_eq!(digit_many("2021/"), Ok(("/", Ok(2021))));
-        assert_eq!(
-            digit_many("/2021/"),
-            Err(nom::error::Error::new(
-                "/2021/",
-                nom::error::ErrorKind::ParseTo
-            ))
-        );
+    fn it_finds_the_date() {
+        assert_eq!(date("20"), Ok(("", String::from("20"))));
+
+        assert_eq!(date("2021"), Ok(("", String::from("2021"))));
+        assert_eq!(date("2021/"), Ok(("/", String::from("2021"))));
+
+        assert_eq!(date("2021-11"), Ok(("", String::from("2021-11"))));
+        assert_eq!(date("2021-11/"), Ok(("/", String::from("2021-11"))));
+
+        assert_eq!(date("2021-01/21"), Ok(("", String::from("2021-1-21"))));
+        assert_eq!(date("2021.01.21/"), Ok(("/", String::from("2021-1-21"))));
     }
 }
