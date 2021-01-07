993f188f893fb0619f455d0ac3e86d1c321af241
Rik Chilvers <rikchilvers@fastmail.com>
2021-01-07 19:19:02 +0000

    Tidy up whitespace

--- src/lexer/whitespace.rs
+++ src/lexer/whitespace.rs
@@ -1,42 +1,10 @@
 use nom::{
-    branch::alt,
-    bytes::complete::tag,
     bytes::complete::take_while,
-    combinator::{map, value, verify},
-    multi::{many0, many1},
-    sequence::terminated,
+    combinator::{map, verify},
     IResult,
 };
 
-/// Counts at least 2 spaces or a tab
-pub fn whitespace(i: &str) -> IResult<&str, Vec<&str>> {
-    terminated(
-        many1(alt((tag("  "), tag("\t"), tag(" \t")))),
-        many0(tag(" ")),
-    )(i)
-}
-
-pub fn whitespace4(i: &str) -> IResult<&str, ()> {
-    value(
-        (),
-        verify(take_while(is_space), |s: &str| {
-            let mut count = 0;
-            for c in s.chars() {
-                match c {
-                    ' ' => count += 1,
-                    '\t' => count += 2,
-                    _ => (),
-                }
-                if count >= 4 {
-                    return true;
-                }
-            }
-            count >= 4
-        }),
-    )(i)
-}
-
-/// Takes at least two spaces
+/// Takes at least two spaces or one tab
 pub fn whitespace2(i: &str) -> IResult<&str, u8> {
     verify(
         map(take_while(is_space), |s: &str| {
@@ -53,21 +21,6 @@ pub fn whitespace2(i: &str) -> IResult<&str, u8> {
         |c| *c >= 2,
     )(i)
 }
-pub fn whitespacex(n: i8, i: &str) -> impl Fn(&str) -> IResult<&str, i8> {
-    move |i: &str| {
-        map(take_while(is_space), |s: &str| {
-            let mut count = 0;
-            for c in s.chars() {
-                match c {
-                    ' ' => count += 1,
-                    '\t' => count += 2,
-                    _ => (),
-                }
-            }
-            count
-        })(i)
-    }
-}
 
 pub fn is_space(chr: char) -> bool {
     nom::character::is_space(chr as u8)
@@ -79,65 +32,35 @@ mod tests {
 
     #[test]
     fn it_handles_tags() {
-        assert_eq!(
-            whitespace("\tsomething else"),
-            Ok(("something else", vec!["\t"]))
-        );
-        assert_eq!(
-            whitespace("\t\tsomething else"),
-            Ok(("something else", vec!["\t", "\t"]))
-        );
+        assert_eq!(whitespace2("\tsomething else"), Ok(("something else", 2)));
+        assert_eq!(whitespace2("\t\tsomething else"), Ok(("something else", 4)));
     }
 
     #[test]
     fn it_handles_spaces() {
-        assert_eq!(
-            // 2 spaces
-            whitespace("  something else"),
-            Ok(("something else", vec!["  "]))
-        );
-        assert_eq!(
-            // 3 spaces
-            whitespace("   something else"),
-            Ok(("something else", vec!["  "]))
-        );
-        assert_eq!(
-            // 4 spaces
-            whitespace("    something else"),
-            Ok(("something else", vec!["  ", "  "]))
-        );
+        // 2 spaces
+        assert_eq!(whitespace2("  something else"), Ok(("something else", 2)));
+        // 3 spaces
+        assert_eq!(whitespace2("   something else"), Ok(("something else", 3)));
+        // 4 spaces
+        assert_eq!(whitespace2("    something else"), Ok(("something else", 4)));
     }
 
     #[test]
     fn it_handles_tags_and_spaces() {
-        assert_eq!(
-            whitespace(" \tsomething else"),
-            Ok(("something else", vec![" \t"]))
-        );
-
-        assert_eq!(
-            whitespace(" \t something else"),
-            Ok(("something else", vec![" \t"]))
-        );
-
-        assert_eq!(
-            whitespace("\t something else"),
-            Ok(("something else", vec!["\t"]))
-        );
-
-        assert_eq!(
-            whitespace("\t  something else"),
-            Ok(("something else", vec!["\t", "  "]))
-        );
+        assert_eq!(whitespace2(" \tsomething else"), Ok(("something else", 3)));
+        assert_eq!(whitespace2(" \t something else"), Ok(("something else", 4)));
+        assert_eq!(whitespace2("\t something else"), Ok(("something else", 3)));
+        assert_eq!(whitespace2("\t  something else"), Ok(("something else", 4)));
     }
 
     #[test]
     fn it_handles_not_enough_whitespace() {
         assert_eq!(
-            whitespace(" something else"),
+            whitespace2(" something else"),
             Err(nom::Err::Error(nom::error::Error::new(
                 " something else",
-                nom::error::ErrorKind::Tag
+                nom::error::ErrorKind::Verify
             )))
         );
     }
