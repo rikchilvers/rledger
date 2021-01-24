c62d705e141cd2b77df4578c4482e5d5b407c504
Rik Chilvers <rikchilvers@fastmail.com>
2021-01-24 21:35:05 +0000

    move some funcs around

--- src/journal/posting.rs
+++ src/journal/posting.rs
@@ -17,3 +17,39 @@ impl Posting {
         self.comments.push(comment)
     }
 }
+
+impl PartialEq for Posting {
+    fn eq(&self, other: &Self) -> bool {
+        let mut equal = false;
+        equal = equal && self.path == other.path;
+        equal = equal && self.amount == other.amount;
+        equal = equal && self.comments == other.comments;
+
+        // TODO: compare transactions
+
+        equal
+    }
+}
+
+impl std::fmt::Display for Posting {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut comments: Option<String> = None;
+        for comment in self.comments.iter() {
+            match comments {
+                Some(c) => comments = Some(format!("{}\n    ; {}", c, comment)),
+                None => comments = Some(format!("    ; {}", comment)),
+            }
+        }
+
+        match &self.amount {
+            Some(a) => match comments {
+                Some(c) => return write!(f, "  {}\t{}\n{}\n", self.path, a, c),
+                None => return write!(f, "  {}\t{}\n", self.path, a),
+            },
+            None => match comments {
+                Some(c) => return write!(f, "  {}\n{}\n", self.path, c),
+                None => return write!(f, "  {}\n", self.path),
+            },
+        }
+    }
+}
--- src/journal/reader/posting.rs
+++ src/journal/reader/posting.rs
@@ -22,29 +22,6 @@ impl From<(&str, Option<Amount>)> for Posting {
     }
 }
 
-impl std::fmt::Display for Posting {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut comments: Option<String> = None;
-        for comment in self.comments.iter() {
-            match comments {
-                Some(c) => comments = Some(format!("{}\n    ; {}", c, comment)),
-                None => comments = Some(format!("    ; {}", comment)),
-            }
-        }
-
-        match &self.amount {
-            Some(a) => match comments {
-                Some(c) => return write!(f, "  {}\t{}\n{}\n", self.path, a, c),
-                None => return write!(f, "  {}\t{}\n", self.path, a),
-            },
-            None => match comments {
-                Some(c) => return write!(f, "  {}\n{}\n", self.path, c),
-                None => return write!(f, "  {}\n", self.path),
-            },
-        }
-    }
-}
-
 /// Returns the indentation level and the posting
 pub fn posting(i: &str) -> IResult<&str, Posting> {
     preceded(
