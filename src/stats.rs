2f97e9d52d8544c43b6cb856ffea8f5e493dd3cc
Rik Chilvers <rikchilvers@fastmail.com>
2021-02-27 19:15:23 +0000

    rework stats to configure reader

--- src/stats.rs
+++ src/stats.rs
@@ -1,15 +1,14 @@
-use crate::command::Command;
-
 use journal::Transaction;
 use reader::error::Error;
 use reader::Date;
 
-use std::sync::Arc;
+use reader::reader::{Config, Reader};
 
 pub struct Statistics {
     start_date: Date,
     end_date: Date,
     transaction_count: usize,
+    posting_count: usize,
 }
 
 impl Statistics {
@@ -18,10 +17,30 @@ impl Statistics {
             start_date: Date::try_from_ymd(100000, 1, 1).unwrap(),
             end_date: Date::try_from_ymd(-100000, 1, 1).unwrap(),
             transaction_count: 0,
+            posting_count: 0,
         }
     }
 
-    fn process_transaction(&mut self, transaction: Arc<Transaction>) {
+    pub fn read(&mut self, file: String) -> Result<(), Error> {
+        let mut reader = Reader::new();
+        let mut config = Config::new();
+
+        let (transactions, postings) = reader.read(file, config)?;
+
+        for t in transactions {
+            self.process_transaction(t);
+        }
+
+        for p in postings {
+            self.posting_count += 1;
+        }
+
+        self.report();
+
+        Ok(())
+    }
+
+    fn process_transaction(&mut self, transaction: Transaction) {
         self.transaction_count += 1;
 
         if transaction.date.lt(&self.start_date) {
@@ -32,27 +51,12 @@ impl Statistics {
             self.end_date = transaction.date;
         }
     }
-}
-
-impl Command for Statistics {
-    fn read_transactions<I>(&mut self, reader: I) -> Result<(), Error>
-    where
-        I: IntoIterator<Item = Result<Arc<Transaction>, Error>>,
-    {
-        for item in reader {
-            match item {
-                Err(e) => return Err(e),
-                Ok(transaction) => self.process_transaction(transaction),
-            }
-        }
-
-        Ok(())
-    }
 
     fn report(&self) {
         println!("First transaction:\t{} (X time ago)", self.start_date);
         println!("Last transaction:\t{} (X time ago)", self.end_date);
         println!("Time period:\t\tXXXX days");
         println!("Transactions:\t\t{} (X.X per day)", self.transaction_count);
+        println!("Postings:\t\t{}", self.posting_count);
     }
 }
