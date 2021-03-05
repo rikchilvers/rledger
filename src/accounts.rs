ee4dd6e7415d8d67df1ed052838ec6d26ba49285
Rik Chilvers <rikchilvers@fastmail.com>
2021-03-05 18:02:38 +0000

    accounts displays a tree

--- src/accounts.rs
+++ src/accounts.rs
@@ -1,26 +1,34 @@
+use journal::Posting;
 use reader::error::Error;
 use reader::reader::{Config, Reader};
 use tree::Tree;
 
 pub struct Accounts<'a> {
     tree: Tree<'a, usize>,
+    postings: Vec<Posting>,
 }
 
 impl<'a> Accounts<'a> {
     pub fn new() -> Self {
-        Self { tree: Tree::new() }
+        Self {
+            tree: Tree::new(),
+            postings: Vec::new(),
+        }
     }
 
-    pub fn read(&mut self, file: String) -> Result<(), Error> {
+    pub fn read(&'a mut self, file: String) -> Result<(), Error> {
         let mut reader = Reader::new();
-        let mut config = Config::new();
-        config.should_sort = true;
+        let config = Config::new();
 
-        let (transactions, postings, _) = reader.read(file, config)?;
+        let (_, postings, _) = reader.read(file, config)?;
+        self.postings = postings;
 
-        for transaction in transactions {
-            transaction.display(&postings);
+        for posting in &self.postings {
+            let mut path: Vec<&str> = posting.path.split(':').collect();
+            self.tree.add_path(&mut path);
         }
+
+        self.tree.display(&None, |_| None);
 
         Ok(())
     }
